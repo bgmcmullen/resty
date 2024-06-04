05da2e15e2200941969ccabfeee178a28e3ceec8
Brendan <bgmcmullen@comcast.net>
2024-06-04 15:03:56 -0400

    restyled header

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './Footer.scss';
 
 function Footer() {
-    return (<footer>&copy; 2024</footer>);
+    return (<footer>&copy; 2024 Brendan McMullen</footer>);
 }
 
 export default Footer;
